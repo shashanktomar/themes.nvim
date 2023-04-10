76419392d7380a4468eda1e93a36ee2b8da9adf7
Shashank Tomar <shashank004@gmail.com>
2023-04-10 16:13:00 +1000

    add colors file

--- tests/colors_spec.lua
+++ tests/colors_spec.lua
@@ -1,27 +1,33 @@
 local colors = require("lua.themes.colors")
 
-describe("hex_to_rgb", function()
+describe("hex2rgb", function()
   it("converts a 6-digit hex color to its RGB values", function()
     local hex_color = "#FF5733"
-    local r, g, b = colors.hex_to_rgb(hex_color)
+    local r, g, b = colors.hex2rgb(hex_color)
     assert.are.equal(255, r)
     assert.are.equal(87, g)
-    assert.are.equal(51, b)
-  end)
-
-  it("converts a 3-digit hex color to its RGB values", function()
-    local hex_color = "#F53"
-    local r, g, b = colors.hex_to_rgb(hex_color)
-    assert.are.equal(255, r)
-    assert.are.equal(85, g)
     assert.are.equal(51, b)
   end)
 
   it("converts a hex color without the '#' symbol", function()
     local hex_color = "FF5733"
-    local r, g, b = colors.hex_to_rgb(hex_color)
+    local r, g, b = colors.hex2rgb(hex_color)
     assert.are.equal(255, r)
     assert.are.equal(87, g)
     assert.are.equal(51, b)
+  end)
+end)
+
+describe("rgb2hex", function()
+  it("converts RGB values to a 6-digit hex color", function()
+    local r, g, b = 255, 87, 51
+    local hex_color = colors.rgb2hex(r, g, b)
+    assert.are.equal("#ff5733", hex_color)
+  end)
+
+  it("converts RGB values with leading zeros to a 6-digit hex color", function()
+    local r, g, b = 0, 127, 16
+    local hex_color = colors.rgb2hex(r, g, b)
+    assert.are.equal("#007f10", hex_color)
   end)
 end)
